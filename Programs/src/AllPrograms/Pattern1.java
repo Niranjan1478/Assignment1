f318542e08cc4a1db3db0cded5b5bf672a6dae04
Niranjan1478 <niranjan.chennaka444@gmail.com>
2022-10-24 23:09:22 +0530

    Assignment1 (Patterns)
    Pattern Programs

---
+++ Programs/src/AllPrograms/Pattern1.java
@@ -0,0 +1,24 @@
+package AllPrograms;
+
+public class Pattern1 
+{
+
+	public static void main(String[] args) 
+	{
+	    int n=14;
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<n;j++)
+	        {
+	            if((i==0)||j==0||i+j<(n-1)/2||j-i>n/2||j==n-1||i==n-1)
+	                System.out.print("* ");
+	            else
+	             System.out.print("  ");
+	        }
+	        System.out.println();
+	    }
+		
+	}
+}
+
+
